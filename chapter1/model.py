85921f5ddf88091f09f5bc1090c92142819716e6
Jouvain Petit-Frere <jpfsmooth@yahoo.com>
2021-08-20 22:14:08 -0400

    First commit with chapter1

---
+++ chapter1/model.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+from typing import Optional
+from datetime import date
+
+@dataclass( repr=True,frozen=True)
+class OrderLine: 
+    orderid: str
+    sku: str
+    qty: int 
+
+class Batch:
+    def __init__( self, ref: str, sku: str, qty: int, eta: Optional[ date] ):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        #self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations = set()
+
+    def allocate( self, line: OrderLine):
+        #self.available_quantity -= line.qty
+        if can_allocate(line):
+            self._allocations.add(line)
+
+    def can_allocate( self, line: OrderLine):
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
+    def deallocate( self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove( line)
+
+    @property
+    def allocated_quantity( self) -> int:
+        return sum( line.qty for line in self._allocations)
+  
+    @property
+    def available_quantity( self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+
+
+
+
+
+if __name__ == '__main__':
+    order = OrderLine("order_id1", "97404378", 40)
+    print(order)
+
+
